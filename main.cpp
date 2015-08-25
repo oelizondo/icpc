463e24ca06d73c0bafbd118b10163498c4c0bb2a
oscarmarcelo95 <oscarmarcelo95@gmail.com>
2015-08-24 19:06:44 -0500

    modifies input

--- main.cpp
+++ main.cpp
@@ -1,45 +1,63 @@
 #include <iostream>
 #include <string>
-#include <fstream>
+
 using namespace std;
 
-struct equipo {
+struct Equipo {
     string nombre;
     char problemas[10];
     int tiempo;
+    int acertados = 0;
     char status;
 };
 
 
 int main()
 {
-    int equipos, cant_problemas, soluciones;
+    int equipos, cant_problemas, soluciones, problemas, tiempo;
+    char tipoProblema, status;
+    string nombre;
 
-    ifstream archEnt;
+    // Inicia el input del programa.
+    cin >> equipos >> cant_problemas;
 
-    archEnt.open("info.txt");
+    //Arreglo struct tipo Equipo
+    Equipo arr_equipos[equipos];
 
+    //Declaración de nombres
+    for (int i = 0; i < arr_equipos.length(); i++){
+      cin >> nombre;
+      arr_equipos[i].nombre = nombre;
+    }
+    //Decalaración de entrega de soluciones
+    cin >> soluciones;
+
+    
     while (!archEnt.eof()){
 
         archEnt >> equipos >> cant_problemas;
 
-        equipo arr_equipos[equipos];
+        Equipo arr_equipos[equipos];
 
-        for (int i = 0 ; i < equipos; i++){
+        for (int i = 0 ; i < equipos; i++)
             archEnt >> arr_equipos[i].nombre;
-            for (int j = 0; j < cant_problemas; i++)
-                archEnt >> arr_equipos[i].problemas[j];
-        }
 
         archEnt >> soluciones;
+
+        while(soluciones){
+          archEnt >> nombre >> tipoProblema >> tiempo >> status;
+
+          for(int i = 0; i < arr_equipos.length(); i++){
+            if (arr_equipos[i].nombre == nombre && status == 'A')
+              arr_equipos[i].tiempo += tiempo;
+              arr_equipos[i].acertados++;
+            else if (arr_equipos[i].nombre == nombre && status 'W')
+          }
+          soluciones--;
+        }
     }
 
     archEnt.close();
-
-    while (soluciones) {
-
-
-    }
 
     return 0;
 }
